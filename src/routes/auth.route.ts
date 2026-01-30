1618c60f45cda6d6c74644e8c51ffb4cc0f41497
qurtubi <qurtubi1411@gmail.com>
2026-01-30 15:48:36 +0700

    add [auth]: validation form

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { login, logout, me, register } from "../controllers/index.js";
 import { auth } from "../middlewares/auth.middleware.js";
+import { validateForm } from "../middlewares/validate.middleware.js";
+import { loginSchema, registerSchema } from "../validations/auth.validation.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateForm(registerSchema), register);
+router.post("/login", validateForm(loginSchema), login);
 router.post("/logout", auth, logout);
 router.get("/me", auth, me);
 export default router;
